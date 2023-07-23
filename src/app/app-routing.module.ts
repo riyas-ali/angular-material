c866774a03d1ded5c93d09c456b6262344241c66
Riyas Ali MK <riyasalimkokl@gmail.com>
2023-07-23 14:52:53 +0530

    added new module for meterials

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,19 @@
-import { NgModule } from '@angular/core';
-import { RouterModule, Routes } from '@angular/router';
+import { NgModule } from "@angular/core";
+import { RouterModule, Routes } from "@angular/router";
+import { ButtonToggleComponent } from "./button-toggle/button-toggle.component";
+import { ButtonsComponent } from "./buttons/buttons.component";
+import { IconComponent } from "./icon/icon.component";
+import { BadgeComponent } from "./badge/badge.component";
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: "", component: ButtonsComponent },
+  { path: "button-toggle", component: ButtonToggleComponent },
+  { path: "icon", component: IconComponent },
+  { path: "badge", component: BadgeComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,30 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from "@angular/core";
+import { BrowserModule } from "@angular/platform-browser";
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
+import { AppRoutingModule } from "./app-routing.module";
+import { AppComponent } from "./app.component";
+import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
+import { MaterialModule } from "./material/material.module";
+import { ButtonToggleComponent } from "./button-toggle/button-toggle.component";
+import { ButtonsComponent } from "./buttons/buttons.component";
+import { IconComponent } from "./icon/icon.component";
+import { BadgeComponent } from "./badge/badge.component";
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ButtonToggleComponent,
+    ButtonsComponent,
+    IconComponent,
+    BadgeComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MaterialModule,
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
